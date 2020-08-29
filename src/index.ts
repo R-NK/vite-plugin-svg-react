c37fbcbd0dcf4572be9bfdb8f8b410ae1a0e850f
R-NK <right.rcon.f@outlook.com>
2020-08-30 01:59:54 +0900

    basic code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello');
+import { Transform } from 'vite';
+
+const reactSvgTransform: Transform = {
+  test: ({ path, query, isBuild }) => {
+    const isSVG = path.endsWith('.svg');
+    if (isSVG) {
+      console.log('isBuild', isBuild);
+      console.log('path', path);
+      console.log('query', query);
+    }
+    return isSVG;
+  },
+  transform: ({ code, path }) => {
+    console.log('code', code);
+    console.log('path', path);
+    return code;
+  },
+};
+
+module.exports = {
+  transforms: [reactSvgTransform],
+};
